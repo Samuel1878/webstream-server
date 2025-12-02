87eab19feacad68fe573d96e0fb241441e54e0b7
Ah San <samuelchost006574@gmail.com>
2025-12-03 00:22:44 +0700

    info

--- controllers/binanceAPIController.mjs
+++ controllers/binanceAPIController.mjs
@@ -1,24 +1,33 @@
 import axios from "axios";
 
 export const getKline = async(req, res,next) => {
-  const { interval } = req.query;
-  // const date = new Date("2025-01-01").getTime()
-  console.log(interval)
-  const response =await axios.get("https://api.binance.com/api/v3/uiKlines", {
-    params:{
-      interval:interval,
-      symbol:req.params.symbol,
-      // startTime:date,
-      // endTime : new Date().getTime()
-    }
-  })
-  if (response.status === 200) {
-    res.json(response.data);
-    return;
-  } else {
-    res.json([]);
+ 
+  try {
+     const { interval } = req.query;
+     // const date = new Date("2025-01-01").getTime()
+     console.log(interval);
+     const response = await axios.get(
+       "https://api.binance.com/api/v3/uiKlines",
+       {
+         params: {
+           interval: interval,
+           symbol: req.params.symbol,
+           // startTime:date,
+           // endTime : new Date().getTime()
+         },
+       }
+     );
+     if (response.status === 200) {
+       res.json(response.data);
+       return;
+     } else {
+       res.json([]);
+     }
+  } catch (error) {
+     res.json([]);
+     console.log(error)
   }
-  next()
+ 
 };
 export const getOrderBook = async (req, res , next) => {
   const response = await axios.get("https://api.binance.com/api/v3/depth", {
@@ -98,5 +107,39 @@ export const getGlobalMarketData = async (req, res, next) => {
     console.log(error);
     res.json(null)
      next();
+  }
+}
+
+export  const getInfoOfEachCoin = async (req, res, next) => {
+  try {
+    const response = await axios.get(
+      `https://api.coinpaprika.com/v1/tickers/${req.params.coin_id}`
+    );
+    if (response.status === 200) {
+      res.json(response.data);
+      return;
+    } else {
+      res.json(null);
+    }
+  } catch (error) {
+    console.log(error);
+    res.json(null);
+  }
+}
+
+export const getLinksOfEachCoin = async (req, res, next) => {
+  try {
+    const response = await axios.get(
+      `https://api.coinpaprika.com/v1/coins/${req.params.coin_id}`
+    );
+    if (response.status === 200) {
+      res.json(response.data);
+      return;
+    } else {
+      res.json(null);
+    }
+  } catch (error) {
+    console.log(error);
+    res.json(null);
   }
 }
--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 import express from "express";
-import { getAggTrade, getAvgPrice, getGlobalMarketData, getKline, getOrderBook, getTickers } from "./controllers/binanceAPIController.mjs";
+import { getAggTrade, getAvgPrice, getGlobalMarketData, getInfoOfEachCoin, getKline, getLinksOfEachCoin, getOrderBook, getTickers } from "./controllers/binanceAPIController.mjs";
 import cors from "cors"
 const app = express();
-const HOST = "150.95.26.121";
+const HOST = "www.auno.site";
 const PORT = 3000
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cors());
 app.get("/", (req, res) => {
-  res.send("Welcome to the Express.js Tutorial");
+  res.send("Welcome to the Binance API Proxy Server");
 });
 app.get("/kline/:symbol", getKline);
 app.get("/orderbook/:symbol", getOrderBook);
 app.get("/avgprice/:symbol", getAvgPrice);
 app.get("/tickers", getTickers);
 app.get("/global", getGlobalMarketData);
-app.get("/aggtrade/:symbol",getAggTrade)
+app.get("/aggtrade/:symbol",getAggTrade);
+app.get("/info/:coin_id", getLinksOfEachCoin);
+app.get("/price/:coin_id", getInfoOfEachCoin);
 
 // Start the server
 app.listen(PORT, () => {
